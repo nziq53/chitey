c9bfb8d9e1fd9a6831125ba151674298ddf3c6a3
oligami-0424 <nziq53@gmail.com>
2023-09-27 15:34:02 +0900

    ファクトリー以外できてきた

---
+++ examples/README.md
@@ -0,0 +1,9 @@
+openssl req -x509 -days 36500 -nodes -newkey rsa:4096 -keyout server.key >server.cert <<-EOT
+> JP
+> Tokyo-to
+> KandaZinbo-tyo
+> Chitey Incorporation
+> Chitey Devision
+> *.chitey.com
+> contact@example.com
+> EOT
--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,12 +1,6 @@
-use chitey::{get, Responder};
+use chitey::{get, Responder, WebServer, post, Certs};
 use bytes::Bytes;
 use http::Response;
-
-#[tokio::main]
-async fn main() {
-    println!("Hello, world!");
-
-}
 
 #[get("/hello/:name")]
 async fn greet(name: String) -> Responder {
@@ -25,43 +19,25 @@ async fn doubb((id, name): (u32, String)) -> Responder {
     let ret = Bytes::copy_from_slice(b"source");
     Ok((builder, ret))
 }
-// use chitey::HttpServiceFactory;
-// use chitey::Resource;
 
-// #[tokio::main]
-// async fn main() {
-//     println!("Hello, world!");
-// }
-// #[allow(non_camel_case_types, missing_docs)]
-// pub struct greet;
-// impl HttpServiceFactory for greet {
-//     fn register(&self) {
-//         async fn greet(name: Path<String>) -> Responder {
-//             {
-//                 println!("Hello {0}!\n", name);
-//             };
-//             let builder = Response::builder();
-//             let ret = Bytes::copy_from_slice(b"source");
-//             Ok((builder, ret))
-//         }
-//         let mut __resource = Resource::new("/hello/{name}");
-//             __resource.regist(greet);
-//     }
-// }
-// #[allow(non_camel_case_types, missing_docs)]
-// pub struct doubb;
-// impl HttpServiceFactory for doubb {
-//     fn register(&self) {
-//         async fn doubb(req: Path<(u32, String)>) -> Responder {
-//             let (id, name) = req.to_owned();
-//             {
-//                 format!("Hello {}! id:{}", name, id);
-//             };
-//             let builder = Response::builder();
-//             let ret = Bytes::copy_from_slice(b"source");
-//             Ok((builder, ret))
-//         }
-//         let mut __resource = Resource::new("/{id}/{name}")
-//             .regist(doubb);
-//     }
-// }
+#[post("/:id/:name")]
+async fn dd((id, name): (u32, String)) -> Responder {
+    format!("Hello {}! id:{}", name, id);
+
+    let builder = Response::builder();
+    let ret = Bytes::copy_from_slice(b"source");
+    Ok((builder, ret))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Hello, world!");
+    WebServer::new()
+    .bind("localhost:18080").unwrap()
+    .tls_bind("localhost:18443").unwrap()
+    .tls(Certs { cert: "server.cert".into(), key: "server.key".into() })
+    .service(greet)
+    .service(doubb)
+    .run()
+    .await
+}
